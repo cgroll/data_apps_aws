fb1a3c37e7d1c3b040b2bb87d8d38a29f9e6cc87
cgroll <christian@scalable.capital>
2020-04-30 11:40:39 +0200

    start notebook rendering job

---
+++ data_apps_aws/src/nb_render.py
@@ -0,0 +1,42 @@
+import re
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
+from nbconvert import HTMLExporter
+from traitlets.config import Config
+from nbconvert.writers import FilesWriter
+
+def scrape_original_trace(trace):
+    ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+    trace = ansi_escape.sub('', trace)
+
+    match = re.search(r'(?<=------------------\n\n).+', trace, re.DOTALL)
+    return match.group(0) if match else trace
+
+def render_nb(nb_name, output_path, jpynb_output_name, tpl_name='./jupyter_hide_code_export.tpl'):
+
+    try:
+
+        # executing the notebook
+        ep = ExecutePreprocessor(timeout=900)
+        nb = nbformat.read(nb_name, nbformat.NO_CONVERT)
+        (nb, resources) = ep.preprocess(nb, resources=dict())
+
+        # transforming it to html
+        html_exporter = HTMLExporter()
+        html_exporter.template_file = tpl_name
+        (body, resources) = html_exporter.from_notebook_node(nb, resources)
+
+        # write it to a file
+        c = Config()
+        c.FilesWriter.build_directory = output_path
+        fw = FilesWriter(config=c)
+        fw.write(body, resources, notebook_name=jpynb_output_name)
+
+    except Exception as e:
+
+        print(f"Exception while rendering {jpynb_name}:")
+        error_msg = scrape_original_trace(e.traceback)
+        # print(e.traceback)
+        # print(error_msg)
+        rethrow_error = True
---
+++ render_notebook/Readme.md
@@ -0,0 +1,5 @@
+Start of a job that:
+
+- downloads a single notebook from github
+- runs and renders it
+- TODO: uploads rendered file to S3 homepage
