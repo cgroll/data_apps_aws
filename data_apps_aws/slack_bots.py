f8415b18fd638537a378fda27f63a2db0959536b
cgroll <christian@scalable.capital>
2020-05-23 18:36:01 +0200

    prettify jupyter slack webhook

--- data_apps_aws/slack_bots.py
+++ data_apps_aws/slack_bots.py
@@ -3,18 +3,22 @@ import json
 
 from data_apps_aws.password_manager import get_db_url
 
+jupyter_logo = "https://logodix.com/logo/1741446.png"
+
 def slack_rendering_error(this_nb):
 
     web_hook_url = get_db_url("slack_bot_webhook")
 
-    slack_msg = {'text': f'Rendering of notebook **{this_nb}** failed. :x:'}
+    slack_msg = {'text': f'Rendering of notebook **{this_nb}** failed. :x:',
+                 'image': jupyter_logo,
+    }
     requests.post(web_hook_url, data=json.dumps(slack_msg))
 
 def slack_status_update(this_nb):
 
     web_hook_url = get_db_url("slack_bot_webhook")
 
-    slack_msg = {'text': f'Notebook **{this_nb}** successfully rendered :heavy_check_mark:'}
+    slack_msg = {'text': f'Notebook *{this_nb}* successfully rendered :heavy_check_mark:'}
     requests.post(web_hook_url, data=json.dumps(slack_msg))
     
 
