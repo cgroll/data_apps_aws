d97d1d1ae6503e3c176987b810cc313170608883
cgroll <christian@scalable.capital>
2020-09-03 14:09:36 +0200

    fix xetra / eurex mix-up

--- data_pipes/econ_fin_data/xetra_eurex_sam/lambda_xetra_1min_data.py
+++ data_pipes/econ_fin_data/xetra_eurex_sam/lambda_xetra_1min_data.py
@@ -1,23 +1,25 @@
 import json
 import pandas
 
-from data_apps_aws.src_data_pipes.fred_data_pipes import update_full_metadata
+from data_apps_aws.src_data_pipes.deutsche_boerse_1min_data import update_data_in_db, get_delayed_xetra_date_to_process
 from data_apps_aws.slack_bots import slack_data_pipe_error, slack_status_update
 
 def lambda_handler(event, context):
     """
-    Lambda function to download Fred Metadata and upload to database.
+    Lambda function to download XETRA and EUREX 1 minute data and upload to database.
     """
 
-    lambda_func_name = 'FredMetadataFunction'
-    data_pipe_logo = "https://www.stlouisfed.org/~/media/Images/Rotator-Images/460x337/Evergreen/Homepage_FRED.jpg"
+    this_date = get_delayed_xetra_date_to_process()
+    lambda_func_name = 'XetraOneMinuteFunction'
+    data_pipe_logo = "https://www.lynxbroker.de/wp-content/uploads/2019/12/xetra-logo.png"
 
-    start_msg = f'Start data processing for *{lambda_func_name}*'
+    start_msg = f'Start data processing for *{lambda_func_name}* and date {this_date}'
     print(start_msg)
     slack_status_update(start_msg, data_pipe_logo)
 
     try:
-        update_full_metadata()
+        
+        update_data_in_db(this_date, 'xetra')
 
         end_msg = f'Data processing successfully done for *{lambda_func_name}*. :heavy_check_mark:'
         print(end_msg)
