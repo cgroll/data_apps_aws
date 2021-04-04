7da3ed59ffa5dda352719b349a8ea693e3e8b3cf
cgroll <christian@scalable.capital>
2021-04-04 07:45:49 +0200

    slackbot output tests

--- data_pipes/econ_fin_data/slack_bot_sam/lambda_slack_bot_request.py
+++ data_pipes/econ_fin_data/slack_bot_sam/lambda_slack_bot_request.py
@@ -1,41 +1,12 @@
 import json
-import pandas
-import logging
-
-from data_apps_aws.slack_bots import slack_data_pipe_error, slack_status_update
 
 def lambda_handler(event, context):
-    """
-    Lambda function to respond to slack bot requests.
-    """
-
-    lambda_func_name = 'SlackBotFunction'
-    logger = logging.getLogger(lambda_func_name)
-    data_pipe_logo = "https://www.shareicon.net/data/256x256/2016/01/05/233432_alfred_256x256.png"
-
-    start_msg = f'Start data processing for *{lambda_func_name}*'
-    print(start_msg)
-    slack_status_update(start_msg, data_pipe_logo)
-
-    try:
-        
-        print('Hello World')
-
-        end_msg = f'Data processing successfully done for *{lambda_func_name}*. :heavy_check_mark:'
-        print(end_msg)
-        slack_status_update(end_msg, data_pipe_logo)
-
-    except Exception as e:
-        logger.error('Failed lambda function with error: '+ str(e))
-
-        end_msg = f'Data processing failed for {lambda_func_name}'
-        print(end_msg)
-        print(str(e))
-        slack_data_pipe_error('AWS_lambda', lambda_func_name, data_pipe_logo)
-
+    print(f"Received event:\n{event}\nWith context:\n{context}")
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": 'Hello World from new slack bot',
+            "text": 'Here is a link: <[https://youtu.be/frszEJb0aOo|General Kenobi!>. :smile: This is a text with `inline *code*` in it.\n- water\n- bread\n - salt,',
+            "message": 'HELLO WORLD',
         }),
     }
