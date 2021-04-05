e7406bc1153a50d8d1d75cca65327168327cbded
cgroll <christian@scalable.capital>
2021-04-05 16:49:41 +0200

    works with general Kenobi output

--- data_pipes/econ_fin_data/slack_bot_flask/app.py
+++ data_pipes/econ_fin_data/slack_bot_flask/app.py
@@ -1,5 +1,8 @@
 import logging
-from flask import Flask
+import json
+from flask import Flask, jsonify
+
+payload = {'blocks': [{'type': 'section', 'text': {'type': 'mrkdwn', 'text': "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*"}}, {'type': 'divider'}, {'type': 'section', 'text': {'type': 'mrkdwn', 'text': '*Farmhouse Thai Cuisine*\n:star::star::star::star: 1528 reviews\n They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here'}, 'accessory': {'type': 'image', 'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/c7ed05m9lC2EmA3Aruue7A/o.jpg', 'alt_text': 'alt text for image'}}, {'type': 'section', 'text': {'type': 'mrkdwn', 'text': '*Kin Khao*\n:star::star::star::star: 1638 reviews\n The sticky rice also goes wonderfully with the caramelized pork belly, which is absolutely melt-in-your-mouth and so soft.'}, 'accessory': {'type': 'image', 'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/korel-1YjNtFtJlMTaC26A/o.jpg', 'alt_text': 'alt text for image'}}, {'type': 'section', 'text': {'type': 'mrkdwn', 'text': '*Ler Ros*\n:star::star::star::star: 2082 reviews\n I would really recommend the  Yum Koh Moo Yang - Spicy lime dressing and roasted quick marinated pork shoulder, basil leaves, chili & rice powder.'}, 'accessory': {'type': 'image', 'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/DawwNigKJ2ckPeDeDM7jAg/o.jpg', 'alt_text': 'alt text for image'}}, {'type': 'divider'}, {'type': 'actions', 'elements': [{'type': 'button', 'text': {'type': 'plain_text', 'text': 'Farmhouse', 'emoji': True}, 'value': 'click_me_123'}, {'type': 'button', 'text': {'type': 'plain_text', 'text': 'Kin Khao', 'emoji': True}, 'value': 'click_me_123', 'url': 'https://google.com'}, {'type': 'button', 'text': {'type': 'plain_text', 'text': 'Ler Ros', 'emoji': True}, 'value': 'click_me_123', 'url': 'https://google.com'}]}]}
 
 app = Flask(__name__)
 logging.basicConfig()
@@ -8,9 +11,19 @@ logger.setLevel(logging.DEBUG)
 
 @app.route('/', methods=['GET', 'POST'])
 def lambda_handler(event=None, context=None):
+    
     logger.info('Lambda function invoked index()')
 
-    return 'Flask says Hello!!'
+    # print(f"Received event:\n{event}\nWith context:\n{context}")
+    # print(event.keys())
+
+    # narrowed_down_text = event['body']
+    # print(f"Somewhere in here is the slack input text: {narrowed_down_text}")
+
+    return jsonify(
+        response_type='in_channel',
+        text='<https://youtu.be/frszEJb0aOo|General Kenobi!>',
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
