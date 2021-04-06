7e2fc7af7e580112ead44a07d312675c40700a80
cgroll <christian@scalable.capital>
2021-04-06 13:49:36 +0200

    make deploy work for the first time

--- slack_slash_commands/slack_bot_sam/lambda_weather.py
+++ slack_slash_commands/slack_bot_sam/lambda_weather.py
@@ -3,11 +3,15 @@ import json
 from data_apps_aws.src_slash_commands.weather import slash_weather, slackify_forecasts
 
 def lambda_handler(event, context):
-    print(f"Received event:\n{event}\nWith context:\n{context}")
-    print(event.keys())
 
-    narrowed_down_text = event['body']
-    print(f"Somewhere in here is the slack input text: {narrowed_down_text}")
+    try:
+        print(f"Received event:\n{event}\nWith context:\n{context}")
+        print(event.keys())
+
+        narrowed_down_text = event['body']
+        print(f"Somewhere in here is the slack input text: {narrowed_down_text}")
+    except:
+        print('Catched error due to unexpected function call')
 
     forecasts, sun_moon_times = slash_weather()
     table_str = slackify_forecasts(forecasts)
