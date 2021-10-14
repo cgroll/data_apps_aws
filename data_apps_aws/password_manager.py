29bc5998f60bbfa1dd4ce492969b7d4ad0ebf839
Christian Groll <christian@scalable.capital>
2021-10-14 09:17:24 +0200

    refactor password manager

--- data_apps_aws/password_manager.py
+++ data_apps_aws/password_manager.py
@@ -49,107 +49,139 @@ def getenv_save(this_env_var):
 IS_DEPLOYED = os.getenv('IS_DEPLOYED', "False")
 
 
-if IS_DEPLOYED == "True":
+def get_service_credentials_cloud(db_name, credentials_type):
 
-    # get passphrase from environment variable
-    gpg_passphrase = getenv_save('GPG_PASSPHRASE')
 
-    db_connections = {
-
-        'quandl': {'user': getenv_save('quandl_user'),
+    if db_name == 'quandl':
+        credentials_dict = {'user': getenv_save('quandl_user'),
                    'api_token': getenv_save('quandl_api_token')
                    },
 
-        'fred': {'api_token': getenv_save('fred_api_token')
+    elif db_name == 'fred':
+        credentials_dict = {'api_token': getenv_save('fred_api_token')
                  },
 
-        'rapidAPI': {'api_token': getenv_save('rapid_api_token')
+    elif db_name == 'rapidAPI':
+        credentials_dict = {'api_token': getenv_save('rapid_api_token')
                  },
 
-        'slack_webhook': {'url': getenv_save('slack_webhook')
+    elif db_name == 'slack_webhook':
+        credentials_dict = {'url': getenv_save('slack_webhook')
                  },
 
-        'slack_cyborg_app': {'api_token': getenv_save('slack_cyborg_app')
+    elif db_name == 'slack_cyborg_app':
+        credentials_dict = {'api_token': getenv_save('slack_cyborg_app')
                  },
 
-        "econ_data": {"user": getenv_save('econ_data_user'),
+    elif db_name == "econ_data":
+        credentials_dict = {"user": getenv_save('econ_data_user'),
                       "password": getenv_save('econ_data_password'),
                       "url": getenv_save('econ_data_url'),
                       },
 
-        "econ_data_read": {"user": getenv_save('econ_data_read_user'),
+    elif db_name == "econ_data_read":
+        credentials_dict = {"user": getenv_save('econ_data_read_user'),
                            "password": getenv_save('econ_data_read_password'),
                            "url": getenv_save('econ_data_read_url'),
                            },
 
-        "bfv_data": {"user": getenv_save('bfv_data_user'),
+    elif db_name == "bfv_data":
+        credentials_dict = {"user": getenv_save('bfv_data_user'),
                           "password": getenv_save('bfv_data_password'),
                           "url": getenv_save('bfv_data_url'),
-                           },
-    }
+                           }
+    
+    else:
+        raise ValueError(f'Not credentials added for service {db_name} yet')
+
+    return credentials_dict[credentials_type]
 
 
-else:
+def get_service_credentials_local(db_name, credentials_type):
 
     # get passphrase from file
     try:
         gpg_passphrase_fname = str(ProjectPaths.secret_path) + '/gpg_passphrase.txt'
         gpg_passphrase = get_passphrase_from_file(gpg_passphrase_fname)
     except:
-        gpg_passphrase = getenv_save('GPG_PASSPHRASE')
+        raise ValueError(f'Reading passphrase from file did not work. Make sure that the path is correct: {gpg_passphrase_fname}')
 
-    db_connections = {
+    if db_name == 'quandl':
 
-        'quandl': {'user': get_credtls('research/quandl/user.gpg', gpg_passphrase),
+        credentials_dict = {'user': get_credtls('research/quandl/user.gpg', gpg_passphrase),
                    'api_token': get_credtls('research/quandl/api_token.gpg', gpg_passphrase)
-                   },
+                   }
 
-        'fred': {'api_token': get_credtls('research/fred/api_token.gpg', gpg_passphrase)
-                 },
+    elif db_name == 'fred':
 
-        'rapidAPI': {'api_token': get_credtls('research/rapidAPI/api_token.gpg', gpg_passphrase)
-                 },
+        credentials_dict = {'api_token': get_credtls('research/fred/api_token.gpg', gpg_passphrase)
+                 }
 
-        'name_prism': {'api_token': get_credtls('research/name_prism/api_token.gpg', gpg_passphrase)
-                 },
+    elif db_name == 'rapidAPI':
 
-        'slack_webhook': {'url': get_credtls('research/slack_webhook/url.gpg', gpg_passphrase)
-                 },
+        credentials_dict = {'api_token': get_credtls('research/rapidAPI/api_token.gpg', gpg_passphrase)
+                 }
 
-        'slack_cyborg_app': {'api_token': get_credtls('research/slack_cyborg_app/api_token.gpg', gpg_passphrase)
-                 },
+    elif db_name == 'name_prism':
+        credentials_dict = {'api_token': get_credtls('research/name_prism/api_token.gpg', gpg_passphrase)
+                 }
 
-        "econ_data": {"user": get_credtls('research/econ_data/user.gpg', gpg_passphrase),
+    elif db_name == 'slack_webhook':
+        credentials_dict = {'url': get_credtls('research/slack_webhook/url.gpg', gpg_passphrase)
+                 }
+
+    elif db_name == 'slack_cyborg_app':
+        credentials_dict = {'api_token': get_credtls('research/slack_cyborg_app/api_token.gpg', gpg_passphrase)
+                 }
+
+    elif db_name == "econ_data":
+        credentials_dict = {"user": get_credtls('research/econ_data/user.gpg', gpg_passphrase),
                       "password": get_credtls('research/econ_data/password.gpg', gpg_passphrase),
                       "url": get_credtls('research/econ_data/url.gpg', gpg_passphrase),
-                      },
-
-        "econ_data_read": {"user": get_credtls('research/econ_data/user.gpg', gpg_passphrase),
+                      }
+        
+    elif db_name == "econ_data_read":
+        credentials_dict = {"user": get_credtls('research/econ_data/user.gpg', gpg_passphrase),
                            "password": get_credtls('research/econ_data/password.gpg', gpg_passphrase),
                            "url": get_credtls('research/econ_data/url.gpg', gpg_passphrase),
-                           },
-
-        "bfv_data": {"user": get_credtls('research/bfv_data/user.gpg', gpg_passphrase),
+                           }
+        
+    elif db_name == "bfv_data":
+        credentials_dict = {"user": get_credtls('research/bfv_data/user.gpg', gpg_passphrase),
                      "password": get_credtls('research/bfv_data/password.gpg', gpg_passphrase),
                      "url": get_credtls('research/bfv_data/url.gpg', gpg_passphrase),
-                      },
-    }
+                      }
+    elif db_name == 'openai':
+        credentials_dict = {"api_token": "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"}
+
+    else:
+        raise ValueError(f'Not credentials added for service {db_name} yet')
+
+    return credentials_dict[credentials_type]
+
+
+def get_service_credentials(db_name, credentials_type):
+
+    if IS_DEPLOYED == "True":
+        return get_service_credentials_cloud(db_name, credentials_type)
+    else:
+        return get_service_credentials_local(db_name, credentials_type)
 
 
 def get_api_token(db_name):
-    return db_connections[db_name]['api_token']
+    return get_service_credentials(db_name, 'api_token')
 
 
 def get_db_url(db_name):
-    return db_connections[db_name]['url']
+    return get_service_credentials(db_name, 'url')
 
 
 def get_db_password(db_name):
-    return db_connections[db_name]['password']
+    return get_service_credentials(db_name, 'password')
 
 
 def get_db_user(db_name):
-    return db_connections[db_name]['user']
+    return get_service_credentials(db_name, 'user')
 
 
 if __name__=="__main__":
